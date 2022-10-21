089320a11d0985d68b583ae18315b79e03c51d5b
HotariTobu <hotari24tools@gmail.com>
2022-10-21 12:21:06 +0900

    initial commit

--- Judge/include/separator.h
+++ Judge/include/separator.h
@@ -13,8 +13,9 @@ Separate map to ground map and building map.
 - map: map of heights
 - ease_of_stay: map of ease of stay values
 [return]
-- 1st value: ground map
-- 2nd value: building map
+- 1st Map2d: ground map
+- 2nd Map2d: building map
+Fill points that do not consist map with nan of double.
 */
 std::pair<Map2d, Map2d> Separate(Map2d map, Map2d ease_of_stay);
 
