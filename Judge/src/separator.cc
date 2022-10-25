b48b08ff41d5a53267c44d8a82273de765db7fc8
guinpen98 <yuri.pocha.0908@gmail.com>
2022-10-25 23:26:34 +0900

    update: Separate()

--- Judge/src/separator.cc
+++ Judge/src/separator.cc
@@ -6,26 +6,26 @@
 
 double separator_threshold;
 
-void InitMap2d(const Map2d& origin, Map2d& init_to_map){
+void InitMap2d(const Map2d<std::pair<double, double>>& origin, Map2d<double>& init_to_map){
     init_to_map.width = origin.width;
     init_to_map.height = origin.height;
-    init_to_map.data = std::vector<std::vector<double>>(init_to_map.height, std::vector<double>(init_to_map.width, std::nan("")));
+    init_to_map.data = std::vector<std::vector<double>>(init_to_map.height + 2, std::vector<double>(init_to_map.width + 2, std::nan("")));
 }
 
 std::pair<Map2d<double>, Map2d<double>> Separate(const Map2d<std::pair<double, double>>& map) {
-    Map2d ground_map;
+    Map2d<double> ground_map;
     InitMap2d(map, ground_map);
 
-    Map2d building_map;
-    InitMap2d(map, ground_map);
+    Map2d<double> building_map;
+    InitMap2d(map, building_map);
 
-    for(int y = 0; y < ease_of_stay.height; y++){
-        for(int x = 0; x < ease_of_stay.width; x++){
-            if(ease_of_stay.data[y][x] > separator_threshold){
-                ground_map.data[y][x]=map.data[y][x];
+    for(int y = 0; y < map.height; y++){
+        for(int x = 0; x < map.width; x++){
+            if(map.data[y][x].second > separator_threshold){
+                ground_map.data[y][x] = map.data[y][x].first;
             }
             else{
-                building_map.data[y][x]=map.data[y][x];
+                building_map.data[y][x] = map.data[y][x].first;
             }
         }
     }
