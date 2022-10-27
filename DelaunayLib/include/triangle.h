ac1a9a7d5da2de771aa9057ac7eabdbf7d223dde
HotariTobu <hotari24tools@gmail.com>
2022-10-27 16:10:44 +0900

    initial commit

--- DelaunayLib/include/triangle.h
+++ DelaunayLib/include/triangle.h
@@ -43,7 +43,13 @@ struct Triangle {
     void Divide(Point3d point);
 
     /*
-    
+    Reconnect a edge if it is illegal.
+    Check `point_indices[1]` - `point_indices[2]`.
+    Define a point that is not in `point_indices` and is in `point_indices` of `neighbors[1]` as D.
+    If a circumscribed circle of the triangle contains D, make new children.
+    One child is consisted of `point_indices[0]`, `point_indices[1]`, and D.
+    Another is consisted of `point_indices[0]`, `point_indices[2]`, and D.
+    Children are assigned into both the triangle and `neighbors[1]`.
     */
     void Flip();
 };
