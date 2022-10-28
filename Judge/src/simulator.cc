850576ee4ef2785a5c4e9feaaeec07400f3dbce9
yu-getu <taka.haru.ys@gmail.com>
2022-10-28 12:21:54 +0900

    doing: Judge_sim

--- Judge/src/simulator.cc
+++ Judge/src/simulator.cc
@@ -2,11 +2,22 @@
 
 #include "../include/simulator.h"
 
+#include <math.h>
+
 double simulator_threshold;
 
 Neighbor Simulate(double value, Neighbor neighbor) {
+
+for(int i; i < 3 ; i++){
+    for(int j; j < 3 ; j++){
+        if(neighbor[i][j] > center && fabs(center-neighbor[i][j]))
+        
+    }
+}
+
 #ifdef __UNIFORM
 // Uniform code is hear...
+
 
 #elif __FLEXIBLE
 // Flexible code is hear...
