54c61746f3bc26e8f0c52d7024f00e1a56974c8a
HotariTobu <hotari24tools@gmail.com>
2022-10-28 12:01:43 +0900

    initial commit

---
+++ Converter/src/ground_points_maker.cc
@@ -0,0 +1,7 @@
+// Created by 
+
+#include "../include/ground_points_maker.h"
+
+Map2d<double> MakeGroundPoints(const Map2d<double>& map) {
+    
+}
---
+++ Converter/src/wrl_writer.cc
@@ -0,0 +1,7 @@
+// Created by 
+
+#include "../include/wrl_writer.h"
+
+void WriteWRL(std::string path, const PointSet& points, const std::vector<std::tuple<int, int, int>> indices) {
+    
+}
