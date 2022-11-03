dbf427e85d3db16d6d87d8ba8c204c8b86114d5b
Hotari <hotari24tools@gmail.com>
2022-11-03 17:12:48 +0900

    Update simulator.cc

--- Judge/src/simulator.cc
+++ Judge/src/simulator.cc
@@ -8,8 +8,24 @@ Neighbor Simulate(double value, Neighbor neighbor) {
 #ifdef __UNIFORM
 // Uniform code is hear...
 
+#ifdef __4_NEIGHBOR
+// Uniform 4-neighbor code is hear...
+
+#elif __8_NEIGHBOR
+// Uniform 8-neighbor code is hear...
+
+#endif
+
 #elif __FLEXIBLE
 // Flexible code is hear...
+
+#ifdef __4_NEIGHBOR
+// Flexible 4-neighbor code is hear...
+
+#elif __8_NEIGHBOR
+// Flexible 8-neighbor code is hear...
+
+#endif
 
 #endif
 }
