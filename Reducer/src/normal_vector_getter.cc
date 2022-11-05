4bc4621bad4cf1bc75255045609d59155b8d02ac
HotariTobu <hotari24tools@gmail.com>
2022-11-05 23:50:37 +0900

    initial commit

--- Reducer/src/normal_vector_getter.cc
+++ Reducer/src/normal_vector_getter.cc
@@ -3,5 +3,13 @@
 #include "../include/normal_vector_getter.h"
 
 Vector3d GetNormalVectorIn(Neighbor neighbor) {
-    
+
+#ifdef __4_EDGE
+// 4-edge code is hear...
+
+#elif __8_EDGE
+// 8-edge code is hear...
+
+#endif
+
 }
