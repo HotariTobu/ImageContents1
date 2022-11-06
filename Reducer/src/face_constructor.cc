34ef80fad2e587972a6339a650bdbcfc4e61ae6c
D0000000000N <114714446+D0000000000N@users.noreply.github.com>
2022-11-07 01:37:31 +0900

    done: Reducer_face

--- Reducer/src/face_constructor.cc
+++ Reducer/src/face_constructor.cc
@@ -1,7 +1,21 @@
-// Created by 
+// Created by
 
 #include "../include/face.h"
 
 Face::Face(const PointVectorSet& set) {
-    
+
+    _points = set.first;
+    f_size = set.first.size();
+    s_size = set.second.size();
+    f_sum = 0;
+    s_sum = 0;
+    for (int i = 0; i < f_size; ++i) {
+        f_sum += i;
+    }
+    for (int i = 0; i < s_size; ++i) {
+        s_sum += i;
+    }
+    _origin = f_sum / f_size;
+    _normal = s_sum / s_size;
+
 }
