76448bac023cdd63e304a2b4e1d51b516e5795cb
neko-ko <88668738+neko-ko@users.noreply.github.com>
2022-11-11 11:39:42 +0900

    fix: neighbor mistakes

--- MapLib/src/neighbor.cc
+++ MapLib/src/neighbor.cc
@@ -4,9 +4,9 @@
 
 template<class T>
 bool operator==(const Neighbor neighbor1, const Neighbor neighbor2) {
-    for (int i - 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            if (neighbor1[i][j] != neighbor2[i][j]) {
+            if (neighbor1.data[i][j] != neighbor2.data[i][j]) {
                 return false;
             }
         }
