e5f699896cfb00f4ae8e8fc30cddf8bbdc03d250
yu-getu <taka.haru.ys@gmail.com>
2022-11-11 12:03:24 +0900

    done:csv_writer

--- MapLib/src/csv_writer.cc
+++ MapLib/src/csv_writer.cc
@@ -9,7 +9,7 @@ void WriteCSV(std::string path, const Map2d<double> &map)
 {
     std::ofstream ofs(path);
     if (ofs){
-        ofs << map.x << "," << (map.width)+x << "," << map.y << "," << (map.height)+y << ",";
+        ofs << map.x << "," << (map.width)+map.x << "," << map.y << "," << (map.height)+map.y << ",";
         for (int i = 1; i < map.height+1; i++){
             for (int j = 1; j < map.width+1; j++){
                 if (!isnan(map.data[i][j])){
