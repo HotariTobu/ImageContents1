d2bc2a521dc6334648ba838ec6a1e8320054b267
HotariTobu <hotari24tools@gmail.com>
2022-11-13 20:32:42 +0900

    add: explanation of uniform mode and flexible mode

--- Judge/include/simulator.h
+++ Judge/include/simulator.h
@@ -11,6 +11,11 @@
 Simulate ease of stay.
 Deal out center value to surroundings.
 If a neighbor value is bigger than the center value and the difference between the center and the neighbor is bigger than `simulator_threshold`, ignore the neighbor.
+
+In uniform mode, the center value is divided by a count of points that can receive ease of stay value.
+
+In flexible mode, first, calculate unsigned differences for each point that can receive ease of stay value. Then, sum them and substruct the sum by each difference. In addition, sum them and divide the differences by the sum. Finally, multiply the center value.
+
 [params]
 - value: ease of stay in center
 - neighbor: surrounding heights
