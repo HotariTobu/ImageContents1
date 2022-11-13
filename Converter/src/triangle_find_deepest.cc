d383fe71a3c0cdd9059abf3da7cb42376258b1b8
HotariTobu <hotari24tools@gmail.com>
2022-11-14 01:27:10 +0900

    improve: not use Triangle::HasChild

--- Converter/src/triangle_find_deepest.cc
+++ Converter/src/triangle_find_deepest.cc
@@ -5,12 +5,11 @@
 Triangle* Triangle::FindDeepest(Point3d* point) const {
     Triangle* triangle = (Triangle*)this;
 
-    while (triangle->HasChild()) {
-        for (int i = 0; i < 3; ++i) {
-            if (triangle->children[i]->Contains(point)) {
-                triangle = triangle->children[i];
-                break;
-            }
+    for (int i = 0; i < 3; ++i) {
+        Triangle* child = triangle->children[i];
+        if (child != nullptr && child->Contains(point)) {
+            triangle = child;
+            i = 0;
         }
     }
 
