0477047ba8a4001656f2588dbb56621b09569c54
HotariTobu <hotari24tools@gmail.com>
2022-11-16 07:38:38 +0900

    fix: map range

--- MapLib/src/csv_writer.cc
+++ MapLib/src/csv_writer.cc
@@ -10,7 +10,7 @@ void WriteCSV(std::string path, const Map2d<double> &map)
 {
     std::ofstream ofs(path);
     if (ofs){
-        ofs << map.x << "," << (map.width+map.x) << "," << map.y << "," << (map.height+map.y) << "\n";
+        ofs << map.x << "," << (map.width+map.x-1) << "," << map.y << "," << (map.height+map.y-1) << "\n";
         for (int i = 1; i < map.height+1; i++){
             for (int j = 1; j < map.width+1; j++){
                 if (!std::isnan(map.data[i][j])){
