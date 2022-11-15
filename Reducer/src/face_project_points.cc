148c9a1c27a359ddf4f437f92df4c071d8994b20
guinpen98 <yuri.pocha.0908@gmail.com>
2022-11-15 15:19:03 +0900

    fix: face_project_points

--- Reducer/src/face_project_points.cc
+++ Reducer/src/face_project_points.cc
@@ -4,6 +4,6 @@
 
 void Face::ProjectPoints() {
     for(auto& point : _points){
-        point.y = _origin.z - ((point.x - _origin.x) * _normal.x + (point.y - _origin.y) * _normal.y) / _normal.z;
+        point.z = _origin.z - (((point.x - _origin.x) * _normal.x + (point.y - _origin.y) * _normal.y) / _normal.z);
     }
 }
