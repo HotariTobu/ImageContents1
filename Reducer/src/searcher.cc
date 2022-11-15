f55c709258ecf8e3a88a0e107cbcab25a39944df
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-11-15 19:44:07 +0900

    fix: Reducer_search

--- Reducer/src/searcher.cc
+++ Reducer/src/searcher.cc
@@ -2,45 +2,39 @@
 
 #include "../include/searcher.h"
 
+#include <cmath>
 #include <queue>
 
 double searcher_threshold;
 
 constexpr int kgroup_nan = -1;
 
-bool IsSafe(const int x, const int y, const int width, const int height)
-{
-    return (x >= 1 && x <= width) && (y >= 1 && y <= height);
-}
-
 double CalculateSimilarity(const Vector3d& vec1, const Vector3d& vec2){
-    return 1 + vec1.Inner(vec2);
+    return vec1.Inner(vec2);
 }
 
 void FloodFill(const Map2d<std::pair<double, Vector3d>>& map, std::vector<std::vector<int>>& group_map, const int x, const int y){
     std::queue<std::pair<int, int>> q;
     q.push({x, y});
-
-    int row[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-    int col[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    std::vector<std::pair<int, int>> points{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
     while(!q.empty()){
         int node_x = q.front().first;
         int node_y = q.front().second;
         q.pop();
 
-        for(int i = 0; i < 8; i++){
-            if (!IsSafe(node_x + row[i], node_y + col[i], map.width, map.height)){
+        for(int i = 0; i < 4; i++){
+            if (std::isnan(map.data[node_y + points[i].first][node_x + points[i].second].first)){
                 continue;
             }
-            if(group_map[node_y + col[i]][node_x + row[i]] != kgroup_nan){
+            if(group_map[node_y + points[i].first][node_x + points[i].second] != kgroup_nan){
                 continue;
             }
-            if(CalculateSimilarity(map.data[node_y][node_x].second, map.data[node_y + col[i]][node_x + row[i]].second) < searcher_threshold){
+            if(CalculateSimilarity(map.data[node_y][node_x].second, map.data[node_y + points[i].first][node_x + points[i].second].second) < searcher_threshold){
                 continue;            
             }
-            group_map[node_y + col[i]][node_x + row[i]] = group_map[node_y][node_x];
-            q.push({node_x + row[i], node_y + col[i]});
+            group_map[node_y +  points[i].first][node_x +  points[i].second] = group_map[node_y][node_x];
+            q.push({node_x +  points[i].second, node_y +  points[i].first});
         }
     }
 }
@@ -63,7 +57,7 @@ std::vector<PointVectorSet> SearchPointGroups(const Map2d<std::pair<double, Vect
     std::vector<PointVectorSet> pointGroups(group_count);
     for(int y = 1; y <= map.height; y++){
         for(int x = 1; x <= map.width; x++){
-            pointGroups[group_map[y][x]].push_back(std::make_pair(Point3d{(double)x, (double)y, map.data[y][x].first}, map.data[y][x].second));
+            pointGroups[group_map[y][x]].push_back(std::make_pair(Point3d{(double)(x - 1), (double)(y - 1), map.data[y][x].first}, map.data[y][x].second));
         }
     }
 
