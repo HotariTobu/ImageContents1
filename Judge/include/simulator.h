95a40461d104cd2f638d21b4087445f590aded6f
Hotari <hotari24tools@gmail.com>
2022-11-15 16:35:21 +0900

    Update simulator.h

--- Judge/include/simulator.h
+++ Judge/include/simulator.h
@@ -14,7 +14,7 @@ If a neighbor value is bigger than the center value and the difference between t
 
 In uniform mode, the center value is divided by a count of points that can receive ease of stay value.
 
-In flexible mode, first, calculate unsigned differences for each point that can receive ease of stay value. Then, sum them and substruct the sum by each difference. In addition, sum them and divide the differences by the sum. Finally, multiply the center value.
+In flexible mode, first, calculate unsigned differences for each point that can receive ease of stay value. Then, sum them and subtract the sum by each difference. In addition, sum them and divide the differences by the sum. Finally, multiply the center ease of stay value.
 
 [params]
 - value: ease of stay in center
