0a46eb2d8e6dd62b3d1beba8d30f42d0294ee273
guinpen98 <yuri.pocha.0908@gmail.com>
2022-11-17 01:25:44 +0900

    fix: triangle_contains

--- Converter/src/triangle_contains.cc
+++ Converter/src/triangle_contains.cc
@@ -2,8 +2,20 @@
 
 #include "../include//triangle.h"
 
+#include <cmath>
+
 bool IsSameSign(const float z1, const float z2){
-    return (z1 > 0 && z2 > 0) || (z1 < 0 && z2 < 0);
+    return (z1 >= 0 && z2 >= 0) || (z1 <= 0 && z2 <= 0);
+}
+
+bool IsNear(const Vector3d& v1, const Vector3d& v2){
+    if (std::fabs(v1.x) < 0.00001){
+        return v2.x < 0.00001;
+    }
+    if (std::fabs(v1.y) < 0.00001){
+        return v2.y < 0.00001;
+    }
+    return std::fabs(v1.x / v2.x - v1.y / v2.y) < 0.00001;
 }
 
 bool Triangle::Contains(Point3d* point) const {
@@ -17,6 +29,16 @@ bool Triangle::Contains(Point3d* point) const {
     ca.z = 0;
     Vector3d cp = *point - *points[2];
     cp.z = 0;
+
+    if(IsNear(bp, ba)){
+        return IsSameSign(bc.Cross(bp).z, ca.Cross(cp).z);
+    }
+    if(IsNear(bc, bp)){
+        return IsSameSign(bp.Cross(ba).z, ca.Cross(cp).z);
+    }
+    if(IsNear(ca, cp)){
+        return IsSameSign(bc.Cross(bp).z, bc.Cross(bp).z);
+    }
     Vector3d outer_products[3] = {bp.Cross(ba), bc.Cross(bp), ca.Cross(cp)};
     return IsSameSign(outer_products[0].z, outer_products[1].z) && IsSameSign(outer_products[1].z, outer_products[2].z);
 }
