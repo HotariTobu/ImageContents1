6f5df6c3e3a44d2e9a24dc6bf897407907990c81
HotariTobu <hotari24tools@gmail.com>
2022-11-17 07:06:52 +0900

    fix: test_ground_points_adder

--- Converter/test/test_ground_points_adder.cc
+++ Converter/test/test_ground_points_adder.cc
@@ -1,64 +1,130 @@
 // Created by 
 
 #include <cassert>
+#include <filesystem>
+#include <limits>
 #include <set>
 
 #include "../include/ground_points_adder.h"
+#include "../include/index_set.h"
+#include "../include/wrl_writer.h"
 
 extern double ground_point_threshold;
 
-bool CanPass(PointSet points, std::vector<IndexSet> indices, std::set<std::set<Point3d>> triangles) {
-    int mm0 = indices.size();
+Map2d<PointType> GetPointTypeMap(const PointSet& points) {
+    int min_x = std::numeric_limits<int>().max();
+    int min_y = std::numeric_limits<int>().max();
+    int max_x = std::numeric_limits<int>().min();
+    int max_y = std::numeric_limits<int>().min();
+
+    for (Point3d point : points) {
+        if (min_x > point.x) {
+            min_x = point.x;
+        }
+        else if (max_x < point.x) {
+            max_x = point.x;
+        }
+
+        if (min_y > point.y) {
+            min_y = point.y;
+        }
+        else if (max_y < point.y) {
+            max_y = point.y;
+        }
+    }
+
+    int width = max_x - min_x + 3;
+    int height = max_y - min_y + 3;
+
+    Map2d<PointType> result;
+    result.x = min_x;
+    result.y = min_y;
+    result.data = std::vector<std::vector<PointType>>(height, std::vector<PointType>(width, PointType::NONE));
+
+    return result;
+}
+
+bool CanPass(PointSet points, std::vector<IndexSet> indices, std::set<std::vector<Point3d>> answer) {
+    int index_set_count = indices.size();
+    auto point_types = GetPointTypeMap(points);
+
+    WriteWRL("before.wrl", points, indices, point_types);
 
     AddGroundPoints(points, indices);
 
-    int mm = indices.size();
-    for (int i = mm0; i < mm; i++) {
-        IndexSet index_set = indices[i];
-        std::set<Point3d> triangle = {
-            points[index_set[0]],
-            points[index_set[1]],
-            points[index_set[2]],
-        };
+    WriteWRL("after.wrl", points, indices, point_types);
 
-        auto ite = triangles.find(triangle);
-        if (ite == triangles.end()) {
-            return false;
+    std::filesystem::remove("before.wrl");
+    std::filesystem::remove("after.wrl");
+
+    std::set<std::vector<Point3d>> result;
+    for (IndexSet index_set : indices) {
+        std::vector<Point3d> point_set;
+
+        for (int i = 0; i < 3; ++i) {
+            point_set.push_back(points[index_set[i]]);
         }
 
-        triangles.erase(ite);
+        result.insert(point_set);
     }
     
-    return triangles.size() == 0;
+    return result == answer;
 }
 
 int main() {
+    ground_point_threshold = 0.5;
+
     assert(CanPass(
         {
             {1, -1, 1},
             {-1, -1, 1},
             {1, 1, 1},
             {-1, 1, 1},
-            {-1, -2, -1},
-            {-2, -1, -1},
+            {1, -2, -1},
+            {2, -1, -1},
         },
         {
-            {3, 2, 4},
-            {3, 1, 2},
-            {6, 5, 2},
-            {5, 1, 2},
-            {6, 2, 4},
+            {0, 5, 2},
+            {0, 2, 3},
+            {0, 3, 1},
+            {0, 1, 4},
+            {0, 4, 5},
         },
         {
             {
-                {1, -1, 1},
-                {-1, -2, -1},
-                {-1, -1, -1},
+                {1, -1, -1},
+                {2, -1, -1},
+                {1, 1, 1},
             },
             {
+                {1, -1, 1},
+                {1, 1, 1},
                 {-1, 1, 1},
-                {-2, -1, -1},
-                {-1, -1, -1},
+            },
+            {
+                {1, -1, 1},
+                {-1, 1, 1},
+                {-1, -1, 1},
+            },
+            {
+                {1, -1, -1},
+                {-1, -1, 1},
+                {1, -2, -1},
+            },
+            {
+                {1, -1, -1},
+                {1, -2, -1},
+                {2, -1, -1},
+            },
+            {
+                {1, -1, -1},
+                {1, 1, 1},
+                {1, -1, 1},
+            },
+            {
+                {1, -1, -1},
+                {1, -1, 1},
+                {-1, -1, 1},
             },
         }
     ));
