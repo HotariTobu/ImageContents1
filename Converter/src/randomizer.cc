3d536b667936a7194d494a69fa405e18dbf33afb
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-11-16 10:16:07 +0900

    done: DelaunayLib_rand

--- Converter/src/randomizer.cc
+++ Converter/src/randomizer.cc
@@ -1,7 +1,12 @@
-// Created by 
+// Created by guinpen98
 
 #include "../include//randomizer.h"
 
+#include <algorithm>
+#include <random>
+
 void Randomize(PointSet& points) {
-    
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+  std::shuffle(points.begin(), points.end(), engine);
 }
