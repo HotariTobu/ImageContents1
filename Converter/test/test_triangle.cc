7656ae9e3411c5d9715e0c76d106b8cb20146f6d
HotariTobu <hotari24tools@gmail.com>
2022-11-16 22:36:25 +0900

    fix: test_triangle

--- Converter/test/test_triangle.cc
+++ Converter/test/test_triangle.cc
@@ -1,67 +1,216 @@
 // Created by 
 
 #include <cassert>
+#include <random>
 #include <set>
 
 #include "../include/triangle.h"
 #include "near.h"
 
+std::default_random_engine random_engine;
+std::uniform_real_distribution<double> random_distribution;
+
+double Rand() {
+    return random_distribution(random_engine);
+}
+
 int main() {
-    Point3d p0 = {0, 0, 0};
-    Point3d p1 = {3, 0, 0};
-    Point3d p2 = {0, 3, 0};
+    std::random_device random_device;
+    random_engine = std::default_random_engine(random_device());
+    random_distribution = std::uniform_real_distribution<double>(-100, 100);
 
-    Point3d p3 = {1, 1, 0};
-    Point3d p4 = {0.6, 0.6, 0};
-    Point3d p5 = {3, 3, 0};
+    Point3d p0 = {0, 0, Rand()};
+    Point3d p1 = {3, 0, Rand()};
+    Point3d p2 = {0, 3, Rand()};
 
-    Triangle t1(&p0, &p1, &p2);
+    Point3d p3 = {1, 1, Rand()};
+    Point3d p4 = {8, 8, Rand()};
+    Point3d p5 = {1.5, 1.5, Rand()};
 
-    Circle incircle = t1.GetIncircle();
-    Circle circumcircle = t1.GetCircumcircle();
+    {
+        Triangle r1(&p0, &p1, &p2);
+        r1.neighbors[0] = &r1;
+        r1.neighbors[1] = &r1;
+        r1.neighbors[2] = &r1;
 
-    assert(Near(incircle.center, {0.878679656440357, 0.878679656440357, 0}) && Near(incircle.radius, 0.87867965644));
-    assert(Near(circumcircle.center, {1.5, 1.5, 0}) && Near(circumcircle.radius, 2.12132034356));
+        assert(!r1.HasChild());
 
-    assert(t1.Contains(&p3));
+        Circle incircle = r1.GetIncircle();
+        Circle circumcircle = r1.GetCircumcircle();
 
-    Triangle t2(&p4, &p0, &p1);
-    Triangle t3(&p4, &p1, &p2);
-    Triangle t4(&p4, &p2, &p0);
+        assert(Near(incircle.center, {0.878679656440357, 0.878679656440357, 0}) && Near(incircle.radius, 0.87867965644));
+        assert(Near(circumcircle.center, {1.5, 1.5, 0}) && Near(circumcircle.radius, 2.12132034356));
 
-    t1.children[0] = &t2;
-    t1.children[1] = &t3;
-    t1.children[2] = &t4;
+        for (double x = 0.1; x < 3; x += 0.1) {
+            Point3d p = {x, 0.1, Rand()};
+            assert(r1.Contains(&p));
+        }
 
-    assert(&t3 == t1.FindDeepest(&p3));
+        for (double y = 0.1; y < 3; y += 0.1) {
+            Point3d p = {0.1, y, Rand()};
+            assert(r1.Contains(&p));
+        }
 
-    Triangle t5(&p0, &p1, &p2);
-    Triangle t6(&p5, &p1, &p2);
+        for (double x = 0.1, y = 2.9; x < 3 && y > 0; x += 0.1, y -= 0.1) {
+            Point3d p = {x, y, Rand()};
+            assert(r1.Contains(&p));
+        }
+    }
 
-    t1.neighbors[1] = &t6;
-    t5.neighbors[1] = &t6;
+    {
+        Triangle r1(&p0, &p1, &p2);
+        r1.neighbors[0] = &r1;
+        r1.neighbors[1] = &r1;
+        r1.neighbors[2] = &r1;
 
-    t1.children[1]->neighbors[1] = &t6;
+        Triangle n1(&p4, &p2, &p1);
+        n1.neighbors[0] = &n1;
+        n1.neighbors[1] = &n1;
+        n1.neighbors[2] = &n1;
 
-    t5.Divide(&p3);
+        r1.neighbors[1] = &n1;
+        n1.neighbors[1] = &r1;
 
-    assert(t1 == t5);
+        Triangle c0(&p3, &p0, &p1);
+        Triangle c1(&p3, &p1, &p2);
+        Triangle c2(&p3, &p2, &p0);
 
-    Triangle t7(&p1, &p3, &p5);
-    Triangle t8(&p2, &p3, &p5);
+        r1.children[0] = &c0;
+        r1.children[1] = &c1;
+        r1.children[2] = &c2;
 
-    t1.children[1]->children[0] = &t7;
-    t1.children[1]->children[1] = &t8;
-    t1.neighbors[1]->children[0] = &t7;
-    t1.neighbors[1]->children[1] = &t8;
+        assert(r1.HasChild());
 
-    t5.children[1]->Flip();
+        assert(&c1 == r1.FindDeepest(&p3));
 
-    assert(Near(t1, t5));
+        c0.neighbors[0] = &c2;
+        c0.neighbors[1] = &r1;
+        c0.neighbors[2] = &c1;
+        c1.neighbors[0] = &c0;
+        c1.neighbors[1] = &n1;
+        c1.neighbors[2] = &c2;
+        c2.neighbors[0] = &c1;
+        c2.neighbors[1] = &r1;
+        c2.neighbors[2] = &c0;
 
-    std::set<Triangle*> leaves = {
-        
-    };
+        Triangle r2(&p0, &p1, &p2);
+        r2.neighbors[0] = &r2;
+        r2.neighbors[1] = &r2;
+        r2.neighbors[2] = &r2;
 
-    assert(t5.GetAllLeaves() == leaves);
+        Triangle n2(&p4, &p2, &p1);
+        n2.neighbors[0] = &n2;
+        n2.neighbors[1] = &n2;
+        n2.neighbors[2] = &n2;
+
+        r2.neighbors[1] = &n2;
+        n2.neighbors[1] = &r2;
+
+        r2.Divide(&p3);
+
+        assert(r1 == r2);
+
+        p4.x = 3;
+        p4.y = 3;
+
+        Triangle c3(&p3, &p1, &p4);
+        Triangle c4(&p3, &p4, &p2);
+
+        c1.children[0] = &c3;
+        c1.children[2] = &c4;
+        n1.children[0] = &c4;
+        n1.children[2] = &c3;
+
+        c3.neighbors[0] = &c0;
+        c3.neighbors[1] = &n1;
+        c3.neighbors[2] = &c4;
+        c4.neighbors[0] = &c3;
+        c4.neighbors[1] = &n1;
+        c4.neighbors[2] = &c2;
+
+        r2.children[1]->Flip();
+
+        assert(r1 == r2);
+    }
+
+    {
+        Triangle r1(&p0, &p1, &p2);
+        r1.neighbors[0] = &r1;
+        r1.neighbors[1] = &r1;
+        r1.neighbors[2] = &r1;
+
+        Triangle n1(&p4, &p2, &p1);
+        n1.neighbors[0] = &n1;
+        n1.neighbors[1] = &n1;
+        n1.neighbors[2] = &n1;
+
+        r1.neighbors[1] = &n1;
+        n1.neighbors[1] = &r1;
+
+        Triangle c0(&p5, &p0, &p1);
+        Triangle c2(&p5, &p2, &p0);
+        Triangle c3(&p5, &p1, &p4);
+        Triangle c4(&p5, &p4, &p2);
+
+        r1.children[0] = &c0;
+        r1.children[2] = &c2;
+        n1.children[0] = &c4;
+        n1.children[2] = &c3;
+
+        c0.neighbors[0] = &c2;
+        c0.neighbors[1] = &r1;
+        c0.neighbors[2] = &c3;
+        c2.neighbors[0] = &c4;
+        c2.neighbors[1] = &r1;
+        c2.neighbors[2] = &c0;
+        c3.neighbors[0] = &c0;
+        c3.neighbors[1] = &n1;
+        c3.neighbors[2] = &c4;
+        c4.neighbors[0] = &c3;
+        c4.neighbors[1] = &n1;
+        c4.neighbors[2] = &c2;
+
+        Triangle r2(&p0, &p1, &p2);
+        r2.neighbors[0] = &r2;
+        r2.neighbors[1] = &r2;
+        r2.neighbors[2] = &r2;
+
+        Triangle n2(&p4, &p2, &p1);
+        n2.neighbors[0] = &n2;
+        n2.neighbors[1] = &n2;
+        n2.neighbors[2] = &n2;
+
+        r2.neighbors[1] = &n2;
+        n2.neighbors[1] = &r2;
+
+        r2.Divide(&p5);
+
+        assert(r1 == r2);
+    }
+
+    {
+        Triangle r2(&p0, &p1, &p2);
+        r2.neighbors[0] = &r2;
+        r2.neighbors[1] = &r2;
+        r2.neighbors[2] = &r2;
+
+        Triangle n2(&p4, &p2, &p1);
+        n2.neighbors[0] = &n2;
+        n2.neighbors[1] = &n2;
+        n2.neighbors[2] = &n2;
+
+        r2.neighbors[1] = &n2;
+        n2.neighbors[1] = &r2;
+
+        r2.Divide(&p3);
+
+        std::set<Triangle*> leaves = {
+            r2.children[0],
+            r2.children[2],
+            n2.children[0],
+            n2.children[2],
+        };
+
+        assert(r2.GetAllLeaves() == leaves);
+    }
 }
