b6ca9f419d6ff5788f47040a7ce6befb4fe9716a
Hotari <hotari24tools@gmail.com>
2022-11-17 10:57:29 +0900

    fix: GetNeighborPointIndex

--- Converter/src/triangle_get_neighbor_point_index.cc
+++ Converter/src/triangle_get_neighbor_point_index.cc
@@ -3,8 +3,16 @@
 #include "../include/triangle.h"
 
 int Triangle::GetNeighborPointIndex(int neighbor_index) {
+    Point3d* point = points[neighbor_index];
     Triangle* neighbor = neighbors[neighbor_index];
-    int j = 0;
-    for (; neighbor->neighbors[j] != this; ++j);
-    return j;
+
+    int j0 = 0;
+    for (; neighbor->points[j0] != point; ++j0);
+
+    int j1 = (j0 + 1) % 3;
+    if (neighbor->points[j1] == points[(neighbor_index + 1) % 3]) {
+        return (j0 + 2) % 3;
+    }
+
+    return j1;
 }
