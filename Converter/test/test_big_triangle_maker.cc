d44f5daa5bd991b57b25639daf2a40833d171901
Hotari <hotari24tools@gmail.com>
2022-11-17 11:06:15 +0900

    fix: test_big_triangle_maker

--- Converter/test/test_big_triangle_maker.cc
+++ Converter/test/test_big_triangle_maker.cc
@@ -1,19 +1,29 @@
 // Created by 
 
 #include <cassert>
+#include <ctime>
 
 #include "../include/big_triangle_maker.h"
 
 #include "../include/triangle.h"
 
-bool CanPass(PointSet points) {
+bool CanPass() {
+    constexpr int points_count = 20;
+    PointSet points(points_count);
+
+    for (int i = 0; i < points_count; i++) {
+        points[i] = {
+            rand(),
+            rand(),
+            rand(),
+        };
+    }
+
     auto [p0, p1, p2] = MakeBigTriangle(points);
     Triangle triangle(&p0, &p1, &p2);
 
-    Circle incircle = triangle.GetIncircle();
-
     for (Point3d point : points) {
-        if ((point - incircle.center).Length() > incircle.radius) {
+        if (!triangle->Contains(point)) {
             return false;
         }
     }
@@ -22,9 +32,9 @@ bool CanPass(PointSet points) {
 }
 
 int main() {
-    assert(CanPass({
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
-    }));
+    srand(std::time(NULL));
+
+    for (int i = 0; i < 5; ++i) {
+        assert(CanPass());
+    }
 }
