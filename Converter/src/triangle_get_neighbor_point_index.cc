5d1b228de493de259c529a78d7025998a9ff5294
HotariTobu <hotari24tools@gmail.com>
2022-11-18 17:06:03 +0900

    fix: GetNeighborPointIndex

--- Converter/src/triangle_get_neighbor_point_index.cc
+++ Converter/src/triangle_get_neighbor_point_index.cc
@@ -10,9 +10,9 @@ int Triangle::GetNeighborPointIndex(int neighbor_index) {
     for (; neighbor->points[j0] != point; ++j0);
 
     int j1 = (j0 + 1) % 3;
-    if (neighbor->points[j1] == points[(neighbor_index + 1) % 3]) {
-        return (j0 + 2) % 3;
-    }
+    // if (neighbor->points[j1] == points[(neighbor_index + 1) % 3]) {
+    //     return (j0 + 2) % 3;
+    // }
 
     return j1;
 }
