adbf34c41a339111016c17c1d1fc94b20a804764
HotariTobu <hotari24tools@gmail.com>
2022-11-19 08:47:15 +0900

    create: tests for circle and contains

---
+++ Converter/test/test_triangle_circle.cc
@@ -0,0 +1,9 @@
+// Created by HotariTobu
+
+#include <cassert>
+
+#include "../include/triangle.h"
+
+int main() {
+    
+}
---
+++ Converter/test/test_triangle_contains.cc
@@ -0,0 +1,11 @@
+// Created by HotariTobu
+
+#include <cassert>
+
+#include "../include/triangle.h"
+
+int main() {
+    Point3d p0 = {0, 0, 0};
+    Point3d p0 = {0, 0, 0};
+    Point3d p0 = {0, 0, 0};
+}
