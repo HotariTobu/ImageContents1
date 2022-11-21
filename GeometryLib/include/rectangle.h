140bd7fe8de119eb116bbab0364568934312e63a
Hotari <hotari24tools@gmail.com>
2022-11-21 13:21:01 +0900

    add: Rectangle

---
+++ GeometryLib/include/rectangle.h
@@ -0,0 +1,15 @@
+// Created by HotariTobu
+
+#ifndef __RECTANGLE_H__
+#define __RECTANGLE_H__
+
+struct Rectangle {
+    double x;
+    double y;
+    double width;
+    double height;
+
+    Rectangle(double x, double y, double width, double height);
+}
+
+#endif // __RECTANGLE_H__
---
+++ GeometryLib/src/rectangle.cc
@@ -0,0 +1,10 @@
+// Created by HotariTobu
+
+#include "../include/rectangle.h"
+
+Rectangle::Rectangle(double x, double y, double width, double height) {
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+}
