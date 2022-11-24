c96430c3e1880d0132934bdd28f1f94c591016b6
HotariTobu <hotari24tools@gmail.com>
2022-11-25 01:26:14 +0900

    fix: convert origin into 3d

--- Reducer/src/face_constructor.cc
+++ Reducer/src/face_constructor.cc
@@ -2,13 +2,21 @@
 
 #include "../include/face.h"
 
+Point3d Get3d(const std::pair<Point2d, double*>& point) {
+    return {
+        point.first.x,
+        point.first.y,
+        *point.second
+    };
+}
+
 Face::Face(const std::list<std::pair<std::pair<Point2d, double*>, const Vector3d*>>& point_vector_list) {
     _origin = {0, 0, 0};
     _normal = {0, 0, 0};
 
     for (auto& e : point_vector_list) {
         _points.push_back(e.first);
-        _origin += e.first;
+        _origin += Get3d(e.first);
         _normal += *e.second;
     }
 
