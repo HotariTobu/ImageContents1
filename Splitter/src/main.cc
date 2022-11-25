0d46cbad2e4c2eff5a44524c62cc3d30bec4c7ee
HotariTobu <hotari24tools@gmail.com>
2022-11-26 00:50:10 +0900

    fix: out of range

--- Splitter/src/main.cc
+++ Splitter/src/main.cc
@@ -1,6 +1,5 @@
 // Created by HotariTobu
 
-#include <cmath>
 #include <iostream>
 #include <map>
 #include <string>
@@ -23,20 +22,25 @@ void process_file(const std::string source_file_path, const std::string destinat
     auto&& [data, rectangle] = ReadDAT<double>(source_file_path);
     ZMap z_map(data, rectangle);
 
-    int columns_count = std::ceil(z_map.width / chunk_length);
-    int rows_count = std::ceil(z_map.height / chunk_length);
+    int columns_count = z_map.width / chunk_length + 1;
+    int rows_count = z_map.height / chunk_length + 1;
 
     auto chunks = std::vector<std::vector<std::map<Point2d, double>>>(columns_count, std::vector<std::map<Point2d, double>>(rows_count));
 
     for (int x = 1; x <= z_map.width; ++x) {
-        for (int y = 1; y <= z_map.width; ++y) {
+        for (int y = 1; y <= z_map.height; ++y) {
             int column = x / chunk_length;
             int row = y / chunk_length;
 
             int index = z_map.GetIndex(x, y);
 
+            auto z_pointer = z_map.z_values[index];
+            if (z_pointer == nullptr) {
+                continue;
+            }
+
             Point2d point = z_map.GetPoint(index);
-            double z = *z_map.z_values[index];
+            double z = *z_pointer;
 
             chunks[column][row].insert({point, z});
         }
@@ -44,7 +48,7 @@ void process_file(const std::string source_file_path, const std::string destinat
     
     for (int column = 0; column < columns_count; ++column) {
         for (int row = 0; row < rows_count; ++row) {
-            std::string chunk_suffix = std::to_string(column) + '-' + std::to_string(row);
+            std::string chunk_suffix = '_' + std::to_string(column) + '-' + std::to_string(row);
             std::string destination_file_path = destination_base_path + filename_suffix + chunk_suffix + ".dat";
             std::cout << "Converting: " << source_file_path << " > " << destination_file_path << std::endl;
 
